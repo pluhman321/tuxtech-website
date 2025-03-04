3d2a6b9bec80221849a6668b4c959912d5e67a9f
TuxTech <hairs.parade_0g@icloud.com>
2025-03-05 12:42:04 +1300

    script.js

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
-// Creates random shooting stars in the background
-document.addEventListener("DOMContentLoaded", () => {
-    const starContainer = document.querySelector(".shooting-stars");
+/* Shooting Stars Effect */
+.shooting-star {
+    position: absolute;
+    width: 100px;
+    height: 2px;
+    background: linear-gradient(-45deg, rgba(95, 145, 255, 1), rgba(0, 0, 255, 0));
+    border-radius: 999px;
+    filter: drop-shadow(0 0 6px rgba(105, 155, 255, 1));
+    animation: tail 3s ease-in-out, shooting 3s ease-in-out;
+}
 
-    function createStar() {
-        let star = document.createElement("div");
-        star.classList.add("shooting-star");
-        star.style.top = Math.random() * window.innerHeight + "px";
-        star.style.left = Math.random() * window.innerWidth + "px";
-        document.body.appendChild(star);
-
-        setTimeout(() => {
-            star.remove();
-        }, 3000);
+@keyframes tail {
+    0% {
+        width: 0;
     }
+    30% {
+        width: 100px;
+    }
+    100% {
+        width: 0;
+    }
+}
 
-    setInterval(createStar, 500);
-});
+@keyframes shooting {
+    0% {
+        transform: translateX(0);
+    }
+    100% {
+        transform: translateX(300px);
+    }
+}
